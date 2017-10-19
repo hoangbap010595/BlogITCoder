b28762b5b1c66283a10bf87d605cbfc1bc07ba8e
hoangbap010595 <lchoang1995@gmail.com>
2017-10-19 15:05:32 +0700

    Commit module Activity

---
+++ Blog.Activity/Areas/Blog/BlogAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Activity.Areas.Blog
+{
+    public class BlogAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Blog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+               name: "Blog_Activity",
+               url: "Blog/Activity/{action}/{id}",
+               defaults: new { controller = "Activity", action = "Activity", id = UrlParameter.Optional },
+               namespaces: new[] { "Blog.Activity.Areas.Blog.Controllers" }
+            );
+        }
+    }
+}
---
+++ Blog.Activity/Areas/Blog/Controllers/ActivityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Activity.Areas.Blog.Controllers
+{
+    [Authorize]
+    public class ActivityController : Controller
+    {
+        // GET: Blog/Activity
+        public ActionResult Activity()
+        {
+            return View();
+        }
+    }
+}
