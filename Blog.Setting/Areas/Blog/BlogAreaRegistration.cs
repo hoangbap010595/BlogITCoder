c0e68d1dedc3863bf22bf56dd473522e102fafa9
hoangbap010595 <lchoang1995@gmail.com>
2017-10-19 17:59:32 +0700

    Commit module Setting

---
+++ Blog.Setting/Areas/Blog/BlogAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Setting.Areas.Blog
+{
+    public class BlogAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Blog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                name: "Blog_Setting",
+                url: "Blog/Setting/{action}/{id}",
+                defaults: new { controller = "Setting", action = "Setting", id = UrlParameter.Optional },
+                namespaces: new[] { "Blog.Setting.Areas.Blog.Controllers" }
+            );
+        }
+    }
+}
---
+++ Blog.Setting/Areas/Blog/Controllers/SettingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Setting.Areas.Blog.Controllers
+{
+    [Authorize]
+    public class SettingController : Controller
+    {
+        // GET: Blog/Setting
+        public ActionResult Setting()
+        {
+            return View();
+        }
+    }
+}
