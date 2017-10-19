b05f9f8c14b711aa7e80cc72425e1fae5acad9dd
hoangbap010595 <lchoang1995@gmail.com>
2017-10-19 14:41:51 +0700

    Commit module Users

---
+++ Blog.Users/Areas/Blog/BlogAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Users.Areas.Blog
+{
+    public class BlogAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Blog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+               name: "Blog_Users",
+               url: "Blog/Users/{action}/{id}",
+               defaults: new { controller = "Users", action = "Users", id = UrlParameter.Optional },
+               namespaces: new[] { "Blog.Users.Areas.Blog.Controllers" }
+            );
+        }
+    }
+}
---
+++ Blog.Users/Areas/Blog/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Users.Areas.Blog.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Blog/Users
+        public ActionResult Users()
+        {
+            return View();
+        }
+    }
+}
