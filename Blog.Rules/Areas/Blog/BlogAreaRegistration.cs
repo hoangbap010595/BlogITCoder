347b78b756680037faf42d8a8adadfe055d5de00
hoangbap010595 <lchoang1995@gmail.com>
2017-10-19 14:51:07 +0700

    Commit module Rules

---
+++ Blog.Rules/Areas/Blog/BlogAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Rules.Areas.Blog
+{
+    public class BlogAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Blog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+               name: "Blog_Rules",
+               url: "Blog/Rules/{action}/{id}",
+               defaults: new { controller = "Rules", action = "Rules", id = UrlParameter.Optional },
+               namespaces: new[] { "Blog.Rules.Areas.Blog.Controllers" }
+            );
+        }
+    }
+}
---
+++ Blog.Rules/Areas/Blog/Controllers/RulesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Rules.Areas.Blog.Controllers
+{
+    [Authorize]
+    public class RulesController : Controller
+    {
+        // GET: Blog/Rules
+        public ActionResult Rules()
+        {
+            return View();
+        }
+    }
+}
